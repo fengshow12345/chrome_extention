e896bff7d1c066006be1e5ddd3505838cbd302a9
zhuhui <zhuhui@zhuanzhuan.com>
2018-07-03 15:22:57 +0800

    update MdPreview 2

--- MdPreview/js/index.js
+++ MdPreview/js/index.js
@@ -1,10 +1,8 @@
 
-window.onload = function() {
-  document.body.innerHTML= "<div id='wrapper'>" + marked(document.querySelector('pre').innerText, {
-    breaks: true,
-    highlight: function(code) {
-      return hljs.highlightAuto(code).value;
-    }
-  }) + "</div>";
-  document.body.style.display = 'block';
-}
+document.body.innerHTML= "<div id='wrapper'>" + marked(document.querySelector('pre').innerText, {
+  breaks: true,
+  highlight: function(code) {
+    return hljs.highlightAuto(code).value;
+  }
+}) + "</div>";
+document.body.style.display = 'block';
